431386a3681fefb5d7ce5bcd644e6baab7fbfbfd
PrinceWilliamLi <814390253@qq.com>
2020-10-26 15:41:22 +0800

    Initial commit

---
+++ src/test/java/com/example/activiti6/Activiti6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.activiti6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activiti6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
